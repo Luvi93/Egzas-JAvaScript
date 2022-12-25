3f0631cb8752eacd5e1621832295413d3bb22f82
Luvi93 <l.vilaniskis@gmail.com>
2022-12-25 22:52:04 +0200

    Task-2

---
+++ Task_2/script.js
@@ -0,0 +1,30 @@
+/* ------------------------------ TASK 2 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri gebės sukurti objektus 3 savybėm ir 1 metodu.
+
+Savybės:
+title: string
+director: string
+budget: number
+
+Metodas: 
+wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
+------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+      this.title = title;
+      this.director = director;
+      this.budget = budget;
+  
+    };
+    wasExpensive = function () {
+     if (this.budget > 100000000) {
+     return true;
+    } 
+      return false;
+    } 
+  };
+  
+  const movieMaker = new Movie('A Walk to Remember', 'A.Strong', 50000000000);
+  
+  movieMaker.wasExpensive();
+  console.log(movieMaker.wasExpensive(), movieMaker.budget + ' USD');
